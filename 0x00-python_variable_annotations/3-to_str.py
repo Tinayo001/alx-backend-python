305f396f0fd8d9e0b2af49ab519d6d84c897f574
Tinayo001 <keiyatinayo@gmail.com>
2024-10-11 15:40:51 +0300

    Basic annotations-to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function that applies type annotations to return a string
+representation of a float number."""
+
+
+def to_str(n: float) -> str:
+    """Function that returns a string representation of a float number.
+
+    Args:
+        number (float): float number.
+
+    Returns:
+        str: string representation of a float number.
+    """
+    return str(n)
