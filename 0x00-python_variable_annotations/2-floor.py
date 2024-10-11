064f7ce5094f7fc6ee34a4638199f3f15d1fc167
Tinayo001 <keiyatinayo@gmail.com>
2024-10-11 15:36:32 +0300

    Basic annotations floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Defines a function that returns floor of a float number."""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function that returns floor of a float number.
+
+    Args:
+        number (float): float number.
+
+    Returns:
+        float: floor of a float number.
+    """
+    return math.floor(n)
